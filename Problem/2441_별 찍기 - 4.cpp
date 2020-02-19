0c1518837ad66cc21d8025c490798b925ea9683e
박관우 <49912860+Gwanwoo-Park@users.noreply.github.com>
2020-02-19 17:07:03 +0900

    Create 2441_별 찍기 - 4.cpp

---
+++ Problem/2441_별 찍기 - 4.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	int i, j;
+
+	scanf("%d", &n);
+
+	for(i=0; i<n; i++)
+	{
+		for(j=0; j<i; j++)
+		{
+			printf(" ");
+		}
+
+		for(j=n; j>i; j--)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
