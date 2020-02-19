637eda9340579a18f28be5e17299970aa874e3f1
박관우 <49912860+Gwanwoo-Park@users.noreply.github.com>
2020-02-19 17:46:53 +0900

    Create 2444_별 찍기 - 7.c

---
+++ Problem/2444_별 찍기 - 7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+main()
+{
+	int i, j;
+	int n;
+
+	scanf("%d", &n);
+
+	for(i=0; i<n; i++)
+	{
+		for(j=i; j<n-1; j++)
+		{
+			printf(" ");
+		}
+		for(j=0; j<2*i+1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for(i=0; i<n-1; i++)
+	{
+		for(j=0; j<i+1; j++) printf(" ");
+		for(j=2*i; j<2*(n-2)+1; j++) printf("*");
+		printf("\n");
+	}
+}
