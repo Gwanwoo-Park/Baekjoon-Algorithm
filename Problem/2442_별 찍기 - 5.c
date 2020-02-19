e1a950c180a961f3d91626217f70561cbe8ac14f
박관우 <49912860+Gwanwoo-Park@users.noreply.github.com>
2020-02-19 17:09:39 +0900

    Create 2442_별 찍기 - 5.c

---
+++ Problem/2442_별 찍기 - 5.c
@@ -0,0 +1,20 @@
+main()
+{
+	int i, j;
+	int n;
+
+	scanf("%d", &n);
+
+	for(i=0; i<n; i++)
+	{
+		for(j=i; j<n-1; j++)
+		{
+			printf(" ");
+		}
+		for(j=0; j<2*i+1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
